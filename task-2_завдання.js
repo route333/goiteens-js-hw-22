c1c6c2aa77fc1d7d4730ca5383ffe176078a895a
Oleg Bondarenko <109847185+route333@users.noreply.github.com>
2023-08-31 19:21:06 +0300

    hw

--- task-2_завдання.js
+++ task-2_завдання.js
@@ -2,13 +2,14 @@ const User = function ({ name, age, followers }) {
   this.name = name;
   this.age = age;
   this.followers = followers;
+
+  this.getInfo = function (name, age, followers) {
+    console.log(
+      `User ${this.name} is ${this.age} years old and has ${this.followers} followers`
+    );
+  };
 }
 
-User.prototype.getInfo = function (name, age, followers) {
-  console.log(
-    `User ${this.name} is ${this.age} years old and has ${this.followers} followers`
-  );
-};
 
 const mango = new User({
   name: "Mango",
